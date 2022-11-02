032b66d79a6aa78f2ede6dcf12d699508cacb1ae
qivvoon <gangjiwon70@gmail.com>
2022-11-02 23:56:51 +0900

    Add log

--- exit_check.py
+++ exit_check.py
@@ -1,13 +1,19 @@
+from log import usual_log
+from log import unusual_log
+
 def next_calculation_check():
     while True:
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation.lower() == "no":
+            usual_log.error("Let's do next calculation? => no")
             return one_more_check()
 
         elif next_calculation.lower() == "yes":
+            usual_log.error("Let's do next calculation? => yes")
             return "continue"
 
         else:
+            usual_log.error("Let's do next calculation? => " + next_calculation)
             continue
 
 
@@ -15,10 +21,13 @@ def one_more_check():
     while True:
         one_more_check = input("Are you sure? (yes/no): ")
         if one_more_check.lower() == "yes":
+            usual_log.error("Are you sure? => yes")
             return "break"
 
         elif one_more_check.lower() == "no":
+            usual_log.error("Are you sure? => no")
             return "continue"
 
         else:
+            usual_log.error("Are you sure? => " + one_more_check)
             continue
